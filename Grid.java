f1e96c7e62c70059873d6d9b0cc832e2e8641016
davas-web-designs <dvd.loge@gmail.com>
2019-07-25 11:53:44 +0200

    DFS

--- Grid.java
+++ Grid.java
@@ -1,5 +1,6 @@
 import java.awt.*;
 import javax.swing.*;
+import java.util.*;
 
 public class Grid{
 
@@ -14,18 +15,21 @@ class drawGrid extends JFrame {
 
 	public drawGrid(){       
 		setSize( 500, 500 );
-		setVisible( true );   
+		setVisible( true );  
+		solve(1,1); 
 	} 
 	public void paint( Graphics g ){
 		int i = 0;
 		for ( int x = 0; x < s*w; x += s ){
 			int j = 0;
-			for ( int y = 0; y < s*w; y += s ){
+			for ( int y = 50; y < s*w + 50; y += s ){
 				//System.out.println(i + " " + j);
 				if(maze[j][i] == 1){
 					g.setColor(Color.YELLOW);
-				}else{
+				}else if(maze[j][i] == 0 || maze[j][i] == 2 || maze[j][i] == 3){
 					g.setColor(Color.RED);
+				}else if(maze[j][i] == 9){
+					g.setColor(Color.BLACK);				
 				}
 				
 				g.fillRect( x, y, s, s);				
@@ -34,19 +38,64 @@ class drawGrid extends JFrame {
 			i++;
 		}	
 	}
+	//DFS:
+	public void solve(int r, int c){
+		if(maze[r][c] != 3){
+			maze[r][c] = 9;
+			ArrayList<IntPair> n = neighbours(r,c);
+			for(IntPair i: n){
+				System.out.println("(" + i.x + " , "+ i.y + ")");
+				if(maze[i.x][i.y] == 0){
+					solve(i.x, i.y);
+				}			
+			}
+		}else{
+			System.out.println("He llegao");		
+		}
+	}
+
+
+	public ArrayList<IntPair> neighbours(int a, int b){
+		ArrayList<IntPair> n = new ArrayList<IntPair>();
+
+		if(maze[a][b+1] == 0){
+			IntPair p = new IntPair(a, b+1);
+			n.add(p);	
+		}
+		if(maze[a][b-1] == 0){
+			IntPair p = new IntPair(a,b-1);
+			n.add(p);
+		}
+		if(maze[a+1][b] == 0){
+			IntPair p = new IntPair(a+1,b);
+			n.add(p);
+		}
+		if(maze[a-1][b] == 0){
+			IntPair p = new IntPair(a-1,b);
+			n.add(p);	
+		}
+		return n;
+	}
+
 	private int s = 30;
 	private int w = 10;
 	private int[][] maze = {
-				{1,0,0,0,1,0,1,0,1,1},
-				{1,1,0,0,1,0,1,0,1,1},
-				{1,0,1,0,0,0,1,0,0,1},
-				{1,0,0,1,1,0,1,0,1,0},
-				{1,0,0,0,1,0,0,0,0,1},
-				{1,0,1,0,0,0,1,0,1,1},
-				{1,0,0,0,1,0,0,1,1,1},
-				{1,0,1,0,0,0,1,0,1,0},
-				{1,0,0,0,1,0,1,0,0,1},
+				{1,1,1,1,1,1,1,1,1,1},
+				{1,2,0,0,0,0,1,1,1,1},
+				{1,0,1,1,1,0,1,1,1,1},
+				{1,0,1,0,0,0,1,1,1,1},
+				{1,0,1,1,1,0,1,1,1,1},
+				{1,0,0,0,0,0,0,0,1,1},
+				{1,1,0,1,1,0,1,0,1,1},
+				{1,1,0,0,0,0,1,0,1,1},
+				{1,1,1,1,1,0,1,0,3,1},
 				{1,1,1,1,1,1,1,1,1,1}
 		};
 	
+}
+
+class IntPair{
+	final int x;
+	final int y;
+	IntPair(int x, int y) {this.x=x;this.y=y;}
 }
--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Exercise to explore pathfinding algorithms in java programming language
 
 ### My plan: 
-1. Get to draw a maze
-2. Get a working algorithm to solve the maze
+1. Get to draw a maze :chart_with_upwards_trend:
+2. Get a working algorithm to solve the maze :space_invader:
 3. At first it will be simple but my go to is the A* or Djikstra's algorithm
 4. When I have my A* or Djikstra working, I would like to make it edcative so that it is possible to see the weights and calculations of eacht step and that sort of things.
 5. Maybe go back and try to work out an algorithm to generate mazes on its own.
 
 All of these things in a Java GUI environment that is very visual.
 
+#### Comments on my progress: 
 
-
+1. The first step was pretty simple, I just did a bi-dimensional array structure to store a bunch of integers which I gave a meaning e.g: 1 is wall, 0 is path, 2 is beggining and 3 is goal...I knew this structure would be the most simple but I also was concerned that there wasnt enough information on it in order to perform the next steps. But anyways I went ahead and drawed it by iterating through it and stablishing a color code.
+2. 
+	* DFS: (DEpth first search) This has been the hardest part so far. I went ahead and took a look at some simple algorithms (actually first I tried to do one simple thing to figure if it was working) and came across DFS and BFS, the pseudo code looked pretty short and the fundation of it, simple and logical, but I had a tough battle with data structures, having to implement an ArrayList of IntPairs, class that I did. It took some faith and brain work, but men, what a satisfaction when it actually worked :blush:
